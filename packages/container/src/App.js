67c08ee1f223507be0b53b3d8b74d4287e697f42
DannaIdarragaR <dannaidarraga@gmail.com>
2022-11-18 07:12:04 -0500

    :lipstick: Fix production styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassname={generateClassname}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
